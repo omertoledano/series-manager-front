1a54ca0c55a1df7850b40f83b978f66ce068940b
Omer Toledano <omertoledano1@gmail.com>
2014-02-15 09:06:41 +0200

    added more functionality
    Signed-off-by: Omer Toledano <omertoledano1@gmail.com>

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,12 +2,18 @@
 
 angular.module('seriesManagerApp')
   .controller('MainCtrl', function ($scope, $firebase) {
-    var promise = new Firebase('https://amber-fire-678.firebaseio.com/');
-    $scope.series = $firebase(promise);
+    $scope.root = new Firebase('https://amber-fire-678.firebaseio.com/');
+    $scope.series = $firebase($scope.root);
     $scope.AddOne = function(show){
         show.episode += 1;
+        $scope.set_episode_server(show.id, show.episode);
     };
     $scope.ReduceOne = function(show){
         show.episode -= 1;
+        $scope.set_episode_server(show.id, show.episode);
+    };
+
+    $scope.set_episode_server = function(id, episode){
+        $scope.root.child(id).child('episode').set(episode);
     };
   });
